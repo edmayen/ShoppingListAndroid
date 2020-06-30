ae728b6f79727738715c19291d302231b5d6f77c
edmayen <ed_mayen@hotmail.com>
2020-06-30 14:13:20 -0500

    Shopping Item, DAO, SQLiteDatabase

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingDAO.kt
@@ -0,0 +1,18 @@
+package com.example.shoppinglist
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface ShoppingDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
